feff0ebe3e887bdf1da2c2350ce15e1af3de1af8
Omitaka555 <takahisa-omichi@nec.com>
2024-10-21 17:58:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,10 +40,7 @@ if ingredients_list:
     st.stop()
     
     time_to_insert = st.button('Submit Order')
-
+    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('your Smoothie is ordered!', icon="✅")
-        
-
-
