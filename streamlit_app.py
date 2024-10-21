29f51aa8dbdfca3c10a59a348e923221bf468dfa
Omitaka555 <takahisa-omichi@nec.com>
2024-10-21 19:13:37 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 import requests
 
 # Write directly to the app
-st.title('My Parents New Healthy Dinner')
+st.title('Customize Your Smoothie')
 st.write(
     """
     Choose the fruits you want in your custom Smoothie!
